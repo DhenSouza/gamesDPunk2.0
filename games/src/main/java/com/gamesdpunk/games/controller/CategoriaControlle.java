55abb76be5e8a4167a6fdd906d183710d2a273de
ShenSouza <ds052798ts@gmail.com>
2021-02-24 20:13:10 -0300

    Post, Put e Delete adicionados no Controller de Categoria

--- games/src/main/java/com/gamesdpunk/games/controller/CategoriaControlle.java
+++ games/src/main/java/com/gamesdpunk/games/controller/CategoriaControlle.java
@@ -3,9 +3,14 @@ package com.gamesdpunk.games.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +39,18 @@ public class CategoriaControlle {
 		return ResponseEntity.ok(repository.findAllByEstiloContainingIgnoreCase(estilo));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
 	
+	@PutMapping
+	public ResponseEntity<Categoria> putCategoria(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteCategoria(@PathVariable long id) {
+		repository.deleteById(id);
+	}
 }
